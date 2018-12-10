1275b8cd4edc9f738aaa546e9f56ed745ec794f4
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-12-10 11:15:20 -0800

    db migration

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,33 @@
 // Update with your config settings.
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = require('./env');
+const { PGHOST, PGDATABASE } = require('./env');
 
+
+// PGUSER, PGPASSWORD
 
 module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: {
-      host: PGHOST,
-      database: PGDATABASE,
-      user: PGUSER,
-      password: PGPASSWORD
-    },
-    migrations: { directory: './db/migrations' },
-    seeds: { directory: './db/seeds' }
-  },
 
   // development: {
   //   client: 'pg',
   //   connection: {
   //     host: PGHOST,
-  //     database: PGDATABASE
+  //     database: PGDATABASE,
+  //     user: PGUSER,
+  //     password: PGPASSWORD
   //   },
   //   migrations: { directory: './db/migrations' },
   //   seeds: { directory: './db/seeds' }
   // },
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: PGHOST,
+      database: PGDATABASE
+    },
+    migrations: { directory: './db/migrations' },
+    seeds: { directory: './db/seeds' }
+  },
 
 
   production: {
